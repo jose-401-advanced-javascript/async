61d6e19e8ea481374417a75b76f0f7357e9b7960
jaojeda <j.a.ojeda@live.com>
2019-09-19 12:49:07 -0700

    tested get method, started  writting get all

--- __tests__/document-collection.test.js
+++ __tests__/document-collection.test.js
@@ -29,5 +29,55 @@ describe('Document Collection', () => {
           expect(writeCalls[0][0]).toBe(`./document/${obj.id}.json`);
         });
     });
+
+    it(`propagates error`, () => {
+      // arrange
+      const error = 'file error';
+      const documentCollection = new DocumentCollection('document');
+      writeFile.mockRejectedValueOnce(error);
+      expect.assertions(1);
+  
+      // act
+      documentCollection.save({ test: 'object' })
+        .catch(err => {
+          expect(err).toBe(error);
+        });
+    });
+  });
+
+  describe('get method', () => {
+
+    it('gets file by id', () => {      
+      const id = 'test';
+      const documentCollection = new DocumentCollection('document');
+      const readPromise = Promise.resolve('{"name":"jose","id":"test"}');
+      readFile.mockResolvedValue(readPromise);
+
+      return documentCollection.get(id)
+        .then(() => {
+
+          const readCalls = readFile.mock.calls;
+          expect(readCalls.length).toBe(1); 
+          expect(readCalls[0][0]).toBe('./document/test.json');
+        });
+    });
+  });
+
+  describe('getAll method', () => {
+
+    it('gets file by id', () => {      
+      const id = 'test';
+      const documentCollection = new DocumentCollection('document');
+      const readPromise = Promise.resolve('{"name":"jose","id":"test"}');
+      readFile.mockResolvedValue(readPromise);
+
+      return documentCollection.get(id)
+        .then(() => {
+
+          const readCalls = readFile.mock.calls;
+          expect(readCalls.length).toBe(1); 
+          expect(readCalls[0][0]).toBe('./document/test.json');
+        });
+    });
   });
 });
