251c52ef4cb17fd8691fea842a315533227a2704
jaojeda <j.a.ojeda@live.com>
2019-09-19 11:01:07 -0700

    wrote and test save method started on get method

--- __tests__/document-collection.test.js
+++ __tests__/document-collection.test.js
@@ -1,12 +1,33 @@
-jest.mock('../lib/files.js', () => ({
-  readFile: jest.fn(),
-  writeFile: jest.fn(),
-  readdir: jest.fn(),
-}));
+jest.mock('../lib/files.js', () => {
+  return {
+    readFile: jest.fn(),
+    writeFile: jest.fn(),
+    readdir: jest.fn(),
+  };
+});
 
 // for setting up mock expectations
 const { readFile, writeFile, readdir } = require('../lib/files');
 
+const DocumentCollection = require('../lib/document-collection');
+
 describe('Document Collection', () => {
   // TODO
+  describe('save method', () => {
+
+    it('saves object', () => {
+      const obj = { name : 'jose' };
+      const documentCollection = new DocumentCollection('document');
+      const writePromise = Promise.resolve(obj);
+      writeFile.mockResolvedValue(writePromise);
+
+      return documentCollection.save(obj)
+        .then(() => {
+
+          const writeCalls = writeFile.mock.calls;
+          expect(writeCalls.length).toBe(1);
+          expect(writeCalls[0][0]).toBe(`./document/${obj.id}.json`);
+        });
+    });
+  });
 });
